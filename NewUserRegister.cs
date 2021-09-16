6474c228c1c2d1a8a3278810f2f7fc37395fc574
Elena.Staneva <Elena.Staneva@SOF-W-L-2109>
2021-09-16 13:50:26 +0300

    Add project files.

---
+++ NewUserRegister.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Moneygaming
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://moneygaming.qa.gameaccount.com/");
+            webDriver.Manage().Window.FullScreen();
+
+            IWebElement JoinNowButton = webDriver.FindElement(By.CssSelector("[href*='/sign-up.shtml']"));
+            JoinNowButton.Click();
+
+            IWebElement TitleDropDown = webDriver.FindElement(By.Id("title"));
+            SelectElement SelectTitle = new SelectElement(TitleDropDown);
+            SelectTitle.SelectByText("Mrs");
+
+            webDriver.FindElement(By.Id("forename")).SendKeys("Elena");
+            webDriver.FindElement(By.Name("map(lastName)")).SendKeys("Staneva");
+            webDriver.FindElement(By.Name("map(terms)")).Click();
+            webDriver.FindElement(By.Id("form")).Click();
+            var ActualErrMessageDoB = webDriver.FindElement(By.XPath("//*[@id='signupForm']/fieldset/label[contains(@for, 'dob')]")).Text;
+            var ExpectedErrMessageDob = "This field is required";
+            Assert.AreEqual(ActualErrMessageDoB, ExpectedErrMessageDob);
+        }
+    }
+}
